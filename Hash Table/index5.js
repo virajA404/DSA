a5db7932c112c73191a3678fa4d509ec3ae7186e
Viraj Abeywardana <abeywardana21@gmail.com>
2023-11-23 17:28:29 +0530

    set

---
+++ Hash Table/index5.js
@@ -0,0 +1,28 @@
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+    }
+    set(key, value){
+      const arr = [];
+      arr.push(key, value);
+      this.data.push(arr);
+      console.log(this.data);
+    }
+    get(key){
+      return this.data.arr[key];
+    }
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+  }
+  
+  const myHashTable = new HashTable(50);
+  myHashTable.set('grapes', 10000)
+  myHashTable.get('grapes')
+  myHashTable.set('apples', 9)
+  // myHashTable.get('apples')
+  
