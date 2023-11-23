55a3c748fc88a2780bf7cde321406b22462510f5
Viraj Abeywardana <abeywardana21@gmail.com>
2023-11-23 23:17:04 +0530

    final answer

--- Hash Table/index5.js
+++ Hash Table/index5.js
@@ -2,32 +2,55 @@ class HashTable {
     constructor(size){
       this.data = new Array(size);
     }
-    set(key, value){
-      const arr = [];
-      arr.push(key, value);
-      this.data.push(arr);
-      console.log(this.data);
-    }
-    get(key){
-      this.data.forEach(element => {
-        if(element[0] === key){
-          console.log(element[1]);
-        }
-      });
-    }
     _hash(key) {
       let hash = 0;
       for (let i =0; i < key.length; i++){
-          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
       }
       return hash;
-    }
+    }//O(1)
+    set(key, value){
+      let address = this._hash(key);
+
+      if(!this.data[address]){
+        this.data[address]=[];
+      }
+      this.data[address].push([key, value]);
+      console.log(this.data);
+      // else{
+      //   for(let address = 0; address < this.data.length; address++){
+      //     if(!this.data[address]){
+      //       this.data[address]=[];
+      //       this.data[address].push(key, value);
+      //       console.log(this.data);
+      //     }
+      //   }
+      // }
+    }//O(1)
+    get(key){
+      let address = this._hash(key);
+      this.data[address].forEach(element => {
+        if(element[0] === key){
+          console.log(element[1]);
+        }else{
+          console.log("Key is not found");
+        }
+      });
+    }//If no collision = O(1) otherwise O(n)
   }
   
-  const myHashTable = new HashTable(50);
+  const myHashTable = new HashTable(2);
   myHashTable.set('grapes', 10000)
-  myHashTable.get('grapes')
-  console.log(myHashTable._hash('grapes')) 
+  // myHashTable.get('grapes')
   myHashTable.set('apples', 9)
-  myHashTable.get('apples')
+  myHashTable.set('mango', 19)
+  myHashTable.set('cherry', 92)
+  myHashTable.set('banana', 12)
+  console.log(myHashTable._hash('apples')) 
+  console.log(myHashTable._hash('grapes')) 
+  console.log(myHashTable._hash('mango')) 
+  console.log(myHashTable._hash('cherry')) 
+  console.log(myHashTable._hash('banana')) 
+
+  myHashTable.get('cherry')
   
