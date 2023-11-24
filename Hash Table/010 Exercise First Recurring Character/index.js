19d583c759f4b7eb72ad785fc000e6546699b4bc
Viraj Abeywardana <abeywardana21@gmail.com>
2023-11-24 23:32:48 +0530

    answer

--- Hash Table/010 Exercise First Recurring Character/index.js
+++ Hash Table/010 Exercise First Recurring Character/index.js
@@ -9,11 +9,34 @@
 //It should return undefined
 
 
-function firstRecurringCharacter(input){
-    
-} 
+// function firstRecurringCharacter(input){
+//   for(let i = 0; i < input.length; i++){
+//     for(let j = i + 1; j < input.length; j++){
+//       if(input[i] === input[j]){
+//         return input[i];
+//       }
+//     }
+//   }
+//   return undefined;
+// }
 
 
+function firstRecurringCharacter(input) {
+  let map = {};
+  for(let i = 0; i < input.length; i++){
+    // console.log(map);
+    if(map[input[i]] !== undefined){
+      return input[i];
+    }else{
+      map[input[i]] = i;
+    }
+    console.log(map);
+  }
+}
+// array = [3,2,5,1,2,3,5,1,2,4];
+array = [2,5,1,1,2,3,5,1,2,4];
+
+console.log(firstRecurringCharacter(array));
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]
 // return 5 because the pairs are before 2,2
