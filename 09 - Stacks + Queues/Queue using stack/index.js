22bd2b7917bea724af3d7f8609b18b3654fe4c55
Viraj Abeywardana <abeywardana21@gmail.com>
2024-01-02 23:15:31 +0530

    creating stack

--- 09 - Stacks + Queues/Queue using stack/index.js
+++ 09 - Stacks + Queues/Queue using stack/index.js
@@ -1,10 +1,39 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+  push(value) {
+    this.array.push(value);
+    return this.array;
+  }
+  pop() {
+    this.array.pop();
+    return this.array;
+  }
+  isEmpty() {
+    if (this.array.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+const stack1 = new Stack();
+const stack2 = new Stack();
+
 class MyQueue {
     constructor() {
-  
+      this.first = null;
+      this.last = null;
+      this.length = 0;
     }
   
     push(x) {
-  
+      
     }
   
     pop() {
@@ -19,4 +48,16 @@ class MyQueue {
   
     }
   }
+
+  // myStack.push("google")
+  // myStack.push("Udemy")
+  // myStack.push("Discord")
+  // console.log(myStack)
   
+  // console.log(myStack.peek());
+  
+  // console.log(myStack.pop())
+  // console.log(myStack.pop())
+  // console.log(myStack.pop())
+  
+  // console.log(myStack.isEmpty())
